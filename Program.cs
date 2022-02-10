785abe6afa78fad27dcc7afc93539e4dceb00756
Dennis Gillich <dennisgillich@live.de>
2022-02-10 16:52:00 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Bitte geben sie ihren Vornamen ein: ");
+string Vorname = Console.ReadLine();
+
+Console.WriteLine("Bitte geben Sie ihren Nachnamen ein: ");
+string Nachname = Console.ReadLine();
+
+Console.WriteLine("Bitte geben Sie ihr Alter ein: ");
+byte Alter = Convert.ToByte(Console.ReadLine());
+
+Console.WriteLine("Hallo " + Vorname + " " + Nachname + ". Sie sind " + Alter + " Jahre alt.");
+Console.ReadKey();
+    
